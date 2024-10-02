f79c99cfe6e9a19bde76db6e83941f703d4facef
lina <linaitider@gmail.com>
2024-10-02 22:28:21 +0100

    First Improvements

---
+++ scripts/functions.js
@@ -0,0 +1,13 @@
+function toggleDarkMode() {
+  document.getElementById('btnSwitch').addEventListener('click',()=>{
+    if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+        document.documentElement.setAttribute('data-bs-theme','light')
+    } else {
+        document.documentElement.setAttribute('data-bs-theme','dark')
+    }
+  })
+}
+
+function imgSlider(anything){
+  document.querySelector('.lipImage').src= anything;
+}
